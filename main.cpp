0adf5cb4375d578f2bce5584d4b8568496e7cc9d
Tom Breza <tom@pcservicegroup.co.uk>
2017-07-06 12:20:24 +0100

    arek class

--- main.cpp
+++ main.cpp
@@ -5,5 +5,30 @@
  */
 
 int main() {
+
+	float a = 5;
+//	double  a = 5;
+//	int a = 5;
+
+	try {
+		a *= 10;
+		if (a == 50)
+			throw a;
+	}
+	catch(int e)
+	{
+		std::cout << "INT A can not be equal to " << a << std::endl;
+	}
+	catch (double e)
+	{
+		std::cout << "Double - A can not be eaqul to " << a << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "This is catch all" << std::endl;
+	}
+	std::cout << "After catch " << std::endl;
+
+
 	return 0;
 }
